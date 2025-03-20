42a72f8388a2159068895a843053465b66a93180
andrew <andrewayiko40@gmail.com>
2025-03-20 21:29:44 +0300

    add AppBar to MyApp home scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(body: const HomePage()),
+      home: Scaffold(appBar: AppBar(), body: const HomePage()),
     );
   }
 }
